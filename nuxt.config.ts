a19ad8a4b661c895a947bfff958ba9c9af951660
yeaayy <154654039+yeaayy@users.noreply.github.com>
2024-04-19 15:53:00 +0700

    Fix github workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/main.scss'],
+  css: [
+    '~/assets/main.scss'
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
